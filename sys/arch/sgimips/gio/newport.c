b542697475353fe73e6702f43bfce62361ac60b2
Adrian Chadd <adrian@FreeBSD.org>
2025-11-27 19:24:02 -0800

    newport: remove extra xmap9_wait() call in xmap9_write_mode()
    This actually breaks being able to write modes, because xmap9_wait()
    is setting DCBMODE/DCBDATA for the fifo wait, overwriting the
    register setup xmap9_write_mode() was setting up!

--- sys/arch/sgimips/gio/newport.c
+++ sys/arch/sgimips/gio/newport.c
@@ -279,8 +279,6 @@ xmap9_write_mode(struct newport_devconfig *dc, uint8_t index, uint32_t mode)
 	    (2 << REX3_DCBMODE_CSHOLD_SHIFT) |
 	    (1 << REX3_DCBMODE_CSSETUP_SHIFT));
 
-	xmap9_wait(dc);
-
 	rex3_write(dc, REX3_REG_DCBDATA0, (index << 24) | mode);
 	junk = rex3_read(dc, REX3_REG_DCBDATA0);
 	__USE(junk);
